e1ee4d229299db52f6b5496f5402bf9bf38e03fb
Kai Blin <kblin@biosustain.dtu.dk>
2021-05-13 21:11:24 +0200

    Make the output prettier
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- js/run.js
+++ js/run.js
@@ -1,9 +1,12 @@
 var inputArea = document.getElementById("csc-input"),
-    outputArea = document.getElementById("variant-calls"),
+    outputTable = document.getElementById("variant-calls"),
     inputDiv = document.getElementById("input"),
+    runningDiv = document.getElementById("running"),
     outputDiv = document.getElementById("output"),
     runButton = document.getElementById("run-button"),
-    resetButton = document.getElementById("reset-button");
+    progressBar = document.getElementById("progress"),
+    resetButton = document.getElementById("reset-button"),
+    tableTemplate = document.getElementById("variant-row");
 
 
 const variantCache = new Map();
@@ -11,7 +14,7 @@ const variantCache = new Map();
 
 function reset() {
     // Clear data
-    outputArea.value = "";
+    outputTable.innerHTML = "";
     inputArea.value = "";
     outputDiv.classList.add("hidden");
     inputDiv.classList.remove("hidden");
@@ -20,6 +23,8 @@ function reset() {
 
 async function run() {
     inputDiv.classList.add("hidden");
+    runningDiv.classList.remove("hidden");
+
     // TODO: show progress bar?
     let csv = inputArea.value;
     let parsed = Papa.parse(csv, { header: true });
@@ -37,25 +42,46 @@ async function run() {
         samples.set(row["sample"], mutations.join(","));
     });
     let variants = await callVariants(samples);
-    let lines = [];
     variants.forEach((variantCall) => {
         let names = [];
         variantCall.variants.forEach((variant) => {
-            names.push(`(${variant.pangolin_lineage}, ${Math.round(variant.proportion * 1000) / 10} %)`);
+            let fixedName = variant.pangolin_lineage[0].toUpperCase() + variant.pangolin_lineage.substr(1);
+            names.push(`${fixedName} (${Math.round(variant.proportion * 1000) / 10} %)`);
         });
-        lines.push(`${variantCall.sample}: ${variantCall.mutations}; ${names.join(", ")}`);
+        let clone = tableTemplate.content.firstElementChild.cloneNode(true);
+        let td = clone.querySelectorAll("td");
+        td[0].textContent = variantCall.sample;
+        td[1].textContent = variantCall.mutations;
+        td[2].textContent = names.join(", ");
+        clone.addEventListener("click", () => {
+            window.open(getSitRepUrl(variantCall.mutations), "_blank");
+        });
+        outputTable.appendChild(clone);
     });
-    outputArea.value = lines.join("\n");
+
+    runningDiv.classList.add("hidden");
     outputDiv.classList.remove("hidden");
+}
+
+
+function getSitRepUrl(mutations) {
+    let mut_components = [];
+    mutations.split(",").forEach((mut) => {
+        mut_components.push(`muts=${encodeURIComponent(mut)}`);
+    });
+    return `https://outbreak.info/situation-reports?pango&${mut_components.join("&")}`;
 }
 
 
 async function callVariants(samples) {
     let variants = [];
+    let counter = 0;
     for (const [sample, mutations] of samples) {
         if (!variantCache.has(mutations)) {
+            counter++;
             let encodedMutations = encodeURIComponent(mutations);
             let result = await fetch(`https://api.outbreak.info/genomics/mutations-by-lineage?mutations=${encodedMutations}`);
+            progressBar.MaterialProgress.setProgress((counter / samples.length) * 100);
             if (!result.ok) {
                 console.error(result.statusText);
                 continue;
@@ -73,7 +99,6 @@ async function callVariants(samples) {
         }
         variants.push({sample: sample, mutations: mutations, variants: variantCache.get(mutations)});
     }
-    console.log(variants);
     return variants;
 }
 
